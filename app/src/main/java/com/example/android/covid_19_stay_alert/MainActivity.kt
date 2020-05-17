676cefe3dec7a36ad58c8ea03ca78603b2236bdd
Antonio <antoniofiori82@yahoo.it>
2020-05-17 18:13:05 +0200

    Init repository

---
+++ app/src/main/java/com/example/android/covid_19_stay_alert/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.covid_19_stay_alert
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
