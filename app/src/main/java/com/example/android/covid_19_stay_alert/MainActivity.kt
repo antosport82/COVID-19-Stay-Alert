314ff3a1755702490487e08c2603fbdb31d5102d
ruchajoshi <joshirucha0807@gmail.com>
2020-05-20 22:56:10 +0100

    Add Main Activity with title and detail fragments.

--- app/src/main/java/com/example/android/covid_19_stay_alert/MainActivity.kt
+++ app/src/main/java/com/example/android/covid_19_stay_alert/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.android.covid_19_stay_alert
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.covid_19_stay_alert.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
